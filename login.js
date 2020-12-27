ecb726d0c581f7e74f43d29ed4753eaf2f11cf50
kirin059 <71425369+kirin059@users.noreply.github.com>
2020-12-27 22:01:17 +0900

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+
+const btn = document.querySelector('.loginBtn');
+const id = document.querySelector('.userId');
+const pw = document.querySelector('.userPw');
+
+
+
+document.addEventListener('keyup', function(){
+  id.value && pw.value 
+  ? (btn.classList.add('active')) 
+  : (btn.classList.remove('active'))  
+}); 
+
+
+
+
